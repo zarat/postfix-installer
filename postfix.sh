c4e491caa964248401c32aede26720299d586456
Manuel Zarat <manuel@zarat.ml>
2025-10-18 16:53:31 +0200

    Configure Postfix installation with debconf and suppress output
    Add debconf selections for Postfix configuration and suppress output during installation.

---
+++ postfix.sh
@@ -0,0 +1,59 @@
+echo "postfix postfix/main_mailer_type string Internet Site" | sudo debconf-set-selections
+echo "postfix postfix/mailname string $(hostname -f)" | sudo debconf-set-selections
+
+sudo DEBIAN_FRONTEND=noninteractive apt install -y postfix > /dev/null 2>&1
+
+# LibSASL installieren
+sudo apt install -y libsasl2-modules sasl2-bin > /dev/null 2>&1
+ 
+# SASL soll gegen PAM authentifizieren. (lokale User)
+sudo sed -i 's/^START=.*/START=yes/' /etc/default/saslauthd
+sudo sed -i 's/^MECHANISMS=.*/MECHANISMS="pam"/' /etc/default/saslauthd
+ 
+sudo systemctl enable --now saslauthd
+sudo systemctl status saslauthd
+ 
+# Postfix user muss in die SASL Gruppe!
+sudo adduser postfix sasl
+ 
+# SASL Konfiguration für Postfix erstellen
+sudo mkdir -p /etc/postfix/sasl
+sudo tee /etc/postfix/sasl/smtpd.conf >/dev/null <<'EOF'
+pwcheck_method: saslauthd
+mech_list: PLAIN LOGIN
+log_level: 7
+EOF
+ 
+# SASL serverseitig einschalten
+sudo postconf -e "smtpd_sasl_auth_enable = yes"
+sudo postconf -e "smtpd_sasl_type = cyrus"
+sudo postconf -e "smtpd_sasl_security_options = noanonymous"
+sudo postconf -e "broken_sasl_auth_clients = yes"
+ 
+# WICHTIG: Keine TLS-Pflicht für AUTH (sonst ohne Zertifikat kein AUTH)
+sudo postconf -e "smtpd_tls_auth_only = no"
+sudo postconf -e "smtpd_use_tls = no"
+sudo postconf -e "smtpd_tls_security_level = none"
+ 
+# Gegen offenes Relay absichern (nur mynetworks ODER authentifiziert)
+sudo postconf -e "smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
+ 
+# Relay Host konfigurieren. Kein AUTH! Postfix dürfen am Exchange anonym senden. Evtl aendern. 
+sudo postconf -e "relayhost = vie-srv-ex02.d2000.local"
+#sudo postconf -e "fallback_relay = vie-srv-ex01.d2000.local"
+ 
+# proxy-protocol konfigurieren
+sudo postconf -e "smtpd_upstream_proxy_protocol = haproxy"
+sudo postconf -e "smtpd_upstream_proxy_timeout = 5s"
+sudo postconf -e "postscreen_upstream_proxy_protocol = haproxy"
+sudo postconf -e "postscreen_upstream_proxy_timeout = 5s"
+ 
+cat <<EOF
+### Add this to master.cf and dont forget to add users!
+ 
+smtp      inet  n       -       n       -       -       smtpd
+  -o smtpd_sasl_auth_enable=yes
+  -o smtpd_upstream_proxy_protocol=haproxy
+127.0.0.1:2525 inet n  -  n  -  -  smtpd
+  -o smtpd_upstream_proxy_protocol=
+EOF
